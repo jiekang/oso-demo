fdc00bea725690f6837a221a740567ac862789cf
Jie Kang <jkang@redhat.com>
2018-04-27 08:37:34 -0400

    init(docker): add Dockerfile and go application example

---
+++ app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello there!")
+}
+
+func main() {
+	fmt.Println("Starting!")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
